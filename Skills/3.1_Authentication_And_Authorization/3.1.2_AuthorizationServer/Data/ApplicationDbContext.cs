7b82290dbd2373e8c305583e905b095aa5ddf0eb
cat fortman <catherine.fortman@gmail.com>
2026-04-18 15:24:44 -0300

    Init authorization server

---
+++ Skills/3.1_Authentication_And_Authorization/3.1.2_AuthorizationServer/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace _3._1._2_AuthorizationServer.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
