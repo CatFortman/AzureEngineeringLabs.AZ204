2b471984d3dbd010ec164dd261dbb1720f15c4ae
cat fortman <catherine.fortman@gmail.com>
2026-04-26 21:01:17 -0300

    Init web app with identity packages

---
+++ Skills/3.1_Authentication_And_Authorization/3.1.4_AppRegistration/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _3._1._4_AppRegistration.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
