154ff0088acec063e705c3f97b2b31cf5c76804c
cat fortman <catherine.fortman@gmail.com>
2026-05-18 22:22:42 -0300

    Update Program.cs

--- Skills/3.1_Authentication_And_Authorization/3.1.4_AppRegistration/Program.cs
+++ Skills/3.1_Authentication_And_Authorization/3.1.4_AppRegistration/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
@@ -7,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpContextAccessor();
 
-builder.Services.AddAuthentication()
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddAuthorization(options =>
